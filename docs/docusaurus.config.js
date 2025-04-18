6702c79d1e3b3ee3cbeac2d83f130b4b24a6564d
MiguelClaret <miguel.claret@sou.inteli.edu.br>
2025-04-17 22:46:55 -0300

    docs: altera nome no projeto

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'miguelclaret', // Usually your GitHub org/user name.
   projectName: 'SeedSafe', // Usually your repo name.
 
   onBrokenLinks: 'warn',
