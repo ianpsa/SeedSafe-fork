2571eba9173ca4931d0b5d4747f26e725aef3163
MiguelClaret <miguel.claret@sou.inteli.edu.br>
2025-04-17 22:39:58 -0300

    docs: altera nome no projeto

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // Set the production url of your site here
    url: 'https://miguelclaret.github.io',
-  baseUrl: '/Seed-Safe/',
+  baseUrl: '/SeedSafe/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
