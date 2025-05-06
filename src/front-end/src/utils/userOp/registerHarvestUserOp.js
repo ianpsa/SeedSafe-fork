ebbc9ac8a047545dd8525f94fc2d47d716aa6438
MariMari0945 <mariana.souza@sou.inteli.edu.br>
2025-05-06 18:01:49 -0300

    feat: last test that worked hahah

--- src/front-end/src/utils/userOp/registerHarvestUserOp.js
+++ src/front-end/src/utils/userOp/registerHarvestUserOp.js
@@ -11,7 +11,13 @@ export const registerHarvestUserOp = async (
 ) => {
   try {
     const contractAddress = CONTRACT_ADDRESSES.harvestManager;
+    const client = await getUserOperationClient();
     const builder = await getSimpleAccountBuilder(signer);
+    builder.setPaymasterOptions({
+      type: 0,
+      apikey: '47cf84d0847b40b59a241cb3ca7dbb6e',
+      rpc: 'https://paymaster-testnet.nerochain.io',
+    })
 
     const contract = new ethers.Contract(contractAddress, HarvestManagerAbi);
     const calldata = contract.interface.encodeFunctionData("createHarvest", [
@@ -25,13 +31,17 @@ export const registerHarvestUserOp = async (
     const userOp = await builder.execute(contractAddress, 0, calldata);
 
     console.log("UserOp gerada:", userOp);
-    console.log("paymasterAndData final:", userOp.paymasterAndData);
+    console.log("paymasterAndData final:", builder.getPaymasterAndData());
 
-    const client = await getUserOperationClient();
+    
     const res = await client.sendUserOperation(userOp);
 
     console.log("UserOperation enviada:", res.userOpHash);
-    return res.userOpHash;
+
+    const receipt = await res.wait();
+    console.log("Receipt:", receipt.transactionHash);
+
+    return receipt.transactionHash;
   } catch (err) {
     console.error("Erro ao enviar UserOp:", err);
     throw err;
